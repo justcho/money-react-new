ab9a90afb6095933305485c40057cead7ee64df9
just <1620845677@qq.com>
2022-07-13 23:25:57 +0800

    收入支出

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,7 @@ import {NavLink} from 'react-router-dom'
 import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
+  background: #fff;
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -18,8 +18,44 @@ const TagsSection = styled.section`
   }
 `
 const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display: flex;
+    align-items: center;
+    >span{ margin-right: 16px; white-space: nowrap }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      border: none;
+      background: none;
+    }
+  }
 `
 const CategorySection = styled.section`
+  font-size: 24px;
+  >ul{
+    display: flex;
+    background: #c4c4c4;
+    >li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected:after{
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `
 const NumberPadSection = styled.section`
 `
@@ -38,12 +74,12 @@ const Money = () => (
     <NotesSection>
       <label>
         <span>备注</span>
-        <input type="text"/>
+        <input type="text" placeholder="在这里添加备注"/>
       </label>
     </NotesSection>
     <CategorySection>
       <ul>
-        <li>支出</li>
+        <li className="selected">支出</li>
         <li>收入</li>
       </ul>
     </CategorySection>
