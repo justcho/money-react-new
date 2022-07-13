ea5acb84c86670f4951c51fcf66a48b82e4534b9
just <1620845677@qq.com>
2022-07-13 23:04:42 +0800

    money 样式

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,24 @@ import Tags from "views/Tags";
 import Money from "./views/Money";
 import Statistics from "./views/Statistics";
 import NoMatch from "./views/NoMatch";
+import styled from "styled-components";
 
+const AppWrapper = styled.div`
+  color: #333;
+`
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/tags" element={<Tags/>}/>
-      <Route path="/money" element={<Money/>}/>
-      <Route path="/statistics" element={<Statistics/>}/>
-      <Route path="/" element={<Money/>}/>
-      <Route path="*" element={<NoMatch/>}/>
-    </Routes>
-  </Router>
+  <AppWrapper>
+    <Router>
+      <Routes>
+        <Route path="/tags" element={<Tags/>}/>
+        <Route path="/money" element={<Money/>}/>
+        <Route path="/statistics" element={<Statistics/>}/>
+        <Route path="/" element={<Money/>}/>
+        <Route path="*" element={<NoMatch/>}/>
+      </Routes>
+    </Router>
+  </AppWrapper>
+
 )
 
 export default App
