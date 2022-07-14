9b9ab6597cae43314d8e1f93e0d4bc659ba82ab6
just <1620845677@qq.com>
2022-07-14 09:59:36 +0800

    标签完成

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const TagsSection = styled.section`
+
+const Wrapper = styled.section`
   background: #fff;padding: 12px 16px;
   flex-grow: 1;
   display: flex;
@@ -12,6 +14,9 @@ const TagsSection = styled.section`
       background: #d9d9d9;border-radius: 18px;
       display: inline-block;padding: 3px 18px;
       font-size: 14px;margin: 8px 12px;
+      &.selected{
+        background: #f60;
+      }
     }
   }
   > button{
@@ -20,5 +25,36 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `
-
+const TagsSection:React.FC =(props)=>{
+  const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
+  const [selected,setSelected] = useState<string[]>([])
+  const addTags = () =>{
+    const tagName = window.prompt('新标签名称为')
+    if(tagName !== null && tagName.length !== 0){
+      setTags([...tags,tagName])
+    }else{
+      window.alert('标签名不为空')
+    }
+  }
+  const toggleTag = (tag:string) =>{
+    const index = selected.indexOf(tag)
+    // 如果他有 就 过滤 了 他
+    if(index>=0){
+      setSelected(selected.filter(t=> t!==tag))
+    }else{
+      setSelected([...selected,tag])
+    }
+  }
+  const getClass =( tag:string) => selected.indexOf(tag)>=0 ? 'selected':''
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={()=>toggleTag(tag)} className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={addTags}>新增标签</button>
+    </Wrapper>
+  )
+}
 export {TagsSection};
