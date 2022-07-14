77a3d74e37a69a8fdcf2145c804d864972e2f166
just <1620845677@qq.com>
2022-07-14 15:02:30 +0800

    numberpad

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
-  >ul{
+
+  > ul {
     display: flex;
     background: #c4c4c4;
-    >li{
+
+    > li {
       width: 50%;
       text-align: center;
       padding: 16px 0;
       position: relative;
-      &.selected:after{
+
+      &.selected:after {
         content: '';
         display: block;
         height: 3px;
@@ -23,4 +27,22 @@ const CategorySection = styled.section`
     }
   }
 `
+const CategorySection: React.FC = () => {
+  const categoryMap = {'-':'支出','+':'收入'}
+  type Keys = keyof typeof categoryMap
+  const [categoryList] = useState<Keys[]>(['-','+'])
+  const [category, setCategory] = useState('-')
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c=>
+            <li key={c} className={category === c ? 'selected' : ''}
+            onClick={()=>setCategory(c)}>
+              {categoryMap[c]}
+            </li>
+          )}
+      </ul>
+    </Wrapper>
+  )
+}
 export {CategorySection}
